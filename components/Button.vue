760e4aeeb0c24317a2388347d48a8adbe7ce92c6
g1azed <victoria1230120@gmail.com>
2023-12-20 03:44:09 +0900

    Button 컴포넌트 제작

---
+++ components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="text-blue-4 bg-white rounded-lg border-blue-4 border-2 font-medium text-sm px-5 py-2.5 me-2 mb-2
+                hover:bg-blue-4 hover:text-white
+                focus:ring-4 focus:ring-blue-300 focus:outline-none"> 
+                <slot> {{ button_txt }}</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props:{
+        // button_txt:{
+        //     type : String,
+        //     default : '',
+        // }
+    }
+}
+</script>
+
+<style></style>
