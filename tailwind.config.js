6f0a50dcb31bb8caf73124f947d6dfe586aff3df
g1azed <victoria1230120@gmail.com>
2023-12-20 21:21:08 +0900

    DNFForgedBlade 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        'gray-666' : '#666666',
-        'black-333' : '#333333',
-        'blue-1' : '#D4EBFD',
-        'blue-2' : '#AAD8FB',
-        'blue-3' : '#7FC4F8',
+        'gray-666': '#666666',
+        'black-333': '#333333',
+        'blue-1': '#D4EBFD',
+        'blue-2': '#AAD8FB',
+        'blue-3': '#7FC4F8',
         'blue-4': '#2A9DF4',
       }
     },
