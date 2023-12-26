14ed0320ed17f1827f0976c9d2e9b1373332d634
taeho <taedonn@taedonn.com>
2023-12-26 09:42:20 +0900

    index.vue 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
-    <div> 
-    
-    </div>
+    <main class="w-full pt-40 flex justify-center">
+        <div class="max-w-[1440px] w-full">
+            <div class="w-full flex items-end text-black-333">
+                <div class="w-full">
+                    <h1 class="mb-3 text-xl leading-none font-medium">타임라인</h1>
+                    <p class="text-base leading-6 font-light">
+                        타임라인은 파티장이 설정한 시간표입니다. <br/>
+                        시간 선택하기를 눌러 타임라인 내 가능한 시간을 선택해 주세요.
+                    </p>
+                </div>
+                <button>시간 선택하기</button>
+            </div>
+            <div class="mt-16 text-black-333">
+                <div class="pl-40">
+                    <h2 class="mb-3 text-xl leading-none font-medium">11/23 풋살하실 분</h2>
+                    <p class="text-base leading-6 font-light">2023년 11월 23일</p>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
+    data: () => ({
+        
+    })
 }
 </script>
 
