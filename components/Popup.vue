5c40430d4dbe3021db31e66358c17a773271c12f
taeho <taedonn@taedonn.com>
2024-01-10 17:56:08 +0900

    Update UI

--- components/Popup.vue
+++ components/Popup.vue
@@ -1,11 +1,11 @@
 <template>
     <div v-show="show" class="w-full h-full fixed left-0 top-0 flex justify-center items-center text-black-333 bg-black-333/20">
-        <div class="w-[1024px] p-16 relative rounded-lg bg-white">
+        <div class="w-[768px] p-14 relative rounded-lg bg-white">
             <button @click="handleClick()" class="absolute right-5 top-5">
                 <i class="text-2xl leading-none fa-solid fa-xmark"></i>
             </button>
             <h2 class="mb-3 text-xl">{{ title }}</h2>
-            <h3 class="mb-10 font-light">{{ subtitle }}</h3>
+            <h3 class="mb-10 text-sm font-light">{{ subtitle }}</h3>
             <slot></slot>
         </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
             <div class="w-full flex items-end text-black-333">
                 <div class="w-full">
                     <h1 class="mb-3 text-xl leading-none font-medium">타임라인</h1>
-                    <p class="text-sm leading-6 font-light">
+                    <p class="text-sm leading-6 font-light text-gray-666">
                         타임라인은 파티장이 설정한 시간표입니다. <br/>
                         시간 선택하기를 눌러 타임라인 내 가능한 시간을 선택해 주세요.
                     </p>
@@ -14,7 +14,7 @@
             <div class="mt-16 text-black-333">
                 <div class="ml-1.5 pl-40">
                     <h2 class="mb-3 text-xl leading-none font-medium">{{ data.title }}</h2>
-                    <p class="text-sm leading-6 font-light">{{ data.date }}</p>
+                    <p class="text-sm leading-6 font-light text-gray-666">{{ data.date }}</p>
                 </div>
                 <div class="w-full mt-5 flex">
                     <Timeline
@@ -41,20 +41,21 @@
             title="시간대 설정"
             subtitle="가능한 시간을 선택해 주세요."
         >
-            <div class="flex gap-7">
-                <Timeline
-                    :timeline="data.timeline"
-                    :checked_time="data.checked_time"
-                />
-                <div class="w-48 shrink-0">
+            <div>
+                <div class="w-48 shrink-0 mb-8">
                     <h2>이름</h2>
                     <input
                         type="text"
                         placeholder="이름을 입력해 주세요."
-                        class="w-full p-2 mt-3 text-sm font-light border-b border-gray-ccc"
+                        class="w-full p-2 mt-3 text-sm font-light border-b border-gray-ccc text-black-333 placeholder-gray-999"
                     />
-                    <button @click="handleShow" class="w-full mt-3 px-4 py-2 leading-none rounded-lg border-2 border-blue-4 text-blue-4">타임라인 추가</button>
                 </div>
+                <Timeline
+                    blank
+                    :timeline="data.timeline"
+                    :checked_time="data.checked_time"
+                />
+                <button @click="handleShow" class="mt-8 px-4 py-2 leading-none rounded-lg border-2 border-blue-4 text-blue-4">타임라인 추가</button>
             </div>
         </Popup>
     </main>
