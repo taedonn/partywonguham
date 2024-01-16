621594f9c628ab671b17428fb04bb5f7cc03e99f
hanjuda <88708708+hanjuda@users.noreply.github.com>
2024-01-17 01:18:46 +0900

    Add files via upload

---
+++ components/Calendar.vue
@@ -0,0 +1,162 @@
+<template>
+    <!-- <div>
+            <p class="calendar-year"></p>년
+            <p class="calendar-month"></p>월
+        </div>
+    <div class="calendar-wrap grid grid-cols-7">
+        <div class="h-100" v-for="weeks in week" :key="weeks">
+            <p class="flex justify-center">{{ weeks }}</p>
+        </div>
+    </div>
+    <div class="calendar-container"> </div> -->
+    <section class="section w-full py-60 flex justify-center">
+        <div class="container flex flex-col justify-center items-center">
+            <div><button @click="handleShow" class="px-[41px] py-[13px] bg-[#2A9DF4] rounded-lg text-white">일정 생성</button></div>
+            <h2 class="subtitle mb-12">
+                <button class="button is-small is-primary is-outlined mr-5" @click="calendarData(-1)">&lt;</button>
+                {{ year }}년 {{ month }}월
+                <button class="button is-small is-primary is-outlined ml-5" @click="calendarData(1)">&gt;</button>
+            </h2>
+            <div class="w-full h-100 flex flex-rows px-16 justify-between">
+                <p  v-for="weeks in week" :key="weeks" class="">{{ weeks }}</p>
+            </div>
+            <table class="table has-text-centered is-fullwidth table-auto">
+                <thead>
+                    <th v-for="day in days" :key="day">{{ day }}</th>
+                </thead>
+                <tbody>
+                    <tr v-for="(date, idx) in dates" :key="idx">
+                        <td v-for="(day, secondIdx) in date" 
+                        :key="secondIdx" 
+                        :class="{ 'text-white': (idx === 0 && day >= lastMonthStart) || (dates.length - 1 === idx && nextMonthStart > day),
+                                'has-text-primary': day === today && month === currentMonth && year === currentYear }"
+                        class="w-48 h-20"
+                        >
+                            <p class="w-full h-full flex justify-center items-center">{{ day }}</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+    <Popup
+        :show="show"
+        :handleClick="handleShow"
+        title="모임제목 설정"
+        subtitle="제목을 설정하지 않아도 랜덤으로 제목이 설정됩니다."
+        >
+            <div>
+                <PartyPop blank/>
+            </div>
+        </Popup>
+</template>
+
+<script>
+import * as DateData from '../utils/date';
+// import * as Date from './Calendarsc';
+
+export default {
+    name: "Calendar",
+    data() {
+        return {
+            show: false,
+            // fetched data
+            data: {},
+            week: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+            // calendarInnerHtml : Date.calendarInnerHtml,
+            // currentMonth : Date.currentMonth,
+            // currentYear : Date.currentYear,
+            dates: [],
+            currentYear: 0,
+            year: 0,
+            month: 0,
+            lastMonthStart: 0,
+            nextMonthStart: 0,
+            today: 0,
+        }
+    },
+    created() {
+        const date = new Date()
+        this.currentYear = date.getFullYear() // 이하 현재 년, 월 가지고 있기
+        this.currentMonth = date.getMonth() + 1
+        this.year = this.currentYear
+        this.month = this.currentMonth
+        this.today = date.getDate() // 오늘 날짜
+        this.calendarData()
+    },
+    methods: {
+        calendarData(arg) {
+            // 인자를 추가
+            if (arg < 0) {
+                // -1이 들어오면 지난 달 달력으로 이동
+                this.month -= 1
+            } else if (arg === 1) {
+                // 1이 들어오면 다음 달 달력으로 이동
+                this.month += 1
+            }
+            if (this.month === 0) {
+                // 작년 12월
+                this.year -= 1
+                this.month = 12
+            } else if (this.month > 12) {
+                // 내년 1월
+                this.year += 1
+                this.month = 1
+            }
+            const [monthFirstDay, monthLastDate, lastMonthLastDate] = this.getFirstDayLastDate(this.year, this.month)
+            this.dates = this.getMonthOfDays(monthFirstDay, monthLastDate, lastMonthLastDate)
+        },
+        getFirstDayLastDate(year, month) {
+            const firstDay = new Date(year, month - 1, 1).getDay() // 이번 달 시작 요일
+            const lastDate = new Date(year, month, 0).getDate() // 이번 달 마지막 날짜
+            let lastYear = year
+            let lastMonth = month - 1
+            if (month === 1) {
+                lastMonth = 12
+                lastYear -= 1
+            }
+            const prevLastDate = new Date(lastYear, lastMonth, 0).getDate() // 지난 달 마지막 날짜
+            return [firstDay, lastDate, prevLastDate]
+        },
+        getMonthOfDays(monthFirstDay, monthLastDate, prevMonthLastDate) {
+            let day = 1
+            let prevDay = prevMonthLastDate - monthFirstDay + 1
+            const dates = []
+            let weekOfDays = []
+            while (day <= monthLastDate) {
+                if (day === 1) {
+                    // 1일이 어느 요일인지에 따라 테이블에 그리기 위한 지난 셀의 날짜들을 구할 필요가 있다.
+                    for (let j = 0; j < monthFirstDay; j += 1) {
+                        if (j === 0) this.lastMonthStart = prevDay // 지난 달에서 제일 작은 날짜
+                        weekOfDays.push(prevDay)
+                        prevDay += 1
+                    }
+                }
+                weekOfDays.push(day)
+                if (weekOfDays.length === 7) {
+                    // 일주일 채우면
+                    dates.push(weekOfDays)
+                    weekOfDays = [] // 초기화
+                }
+                day += 1
+            }
+            const len = weekOfDays.length
+            if (len > 0 && len < 7) {
+                for (let k = 1; k <= 7 - len; k += 1) {
+                    weekOfDays.push(k)
+                }
+            }
+            if (weekOfDays.length > 0) dates.push(weekOfDays) // 남은 날짜 추가
+            this.nextMonthStart = weekOfDays[0] // 이번 달 마지막 주에서 제일 작은 날짜
+            return dates
+        },
+        handleShow() { this.show = !this.show; }
+    },
+}
+</script>
+
+<style>
+.calendar-wrap {
+    margin-top: 200px;
+}
+</style>
