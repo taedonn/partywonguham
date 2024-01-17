37183218bdf513e83b1f2883d267aa49f4fa5baa
g1azed <victoria1230120@gmail.com>
2024-01-17 19:27:05 +0900

    Calendar 컴포넌트 기능
    - 지난 날짜 클릭X, text-color: gray 변경
    - index 페이지 UI

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -1,15 +1,12 @@
 <template>
-    <!-- <div>
-            <p class="calendar-year"></p>년
-            <p class="calendar-month"></p>월
+    <section class="section w-full py-60 flex flex-col justify-center items-center gap-y-14">
+        <div class="container flex flex-row justify-between">
+            <div>
+                <h2 class="text-xl font-medium text-black-333 mb-3">언제 약속을 잡고 싶으신가요?</h2>
+                <p class="text-base font-light text-black-333">일정을 잡고, 인원수를 설정한 후 생성된 타임라인을 지인들과 공유해 보세요. <br />타임라인 내 특정 시간에 인원수가 채워지면 이메일로 알림이 발송됩니다.</p>
+            </div>
+            <div class="flex justify-items-end items-center"> <Button> 일정 생성하기 </Button></div>
         </div>
-    <div class="calendar-wrap grid grid-cols-7">
-        <div class="h-100" v-for="weeks in week" :key="weeks">
-            <p class="flex justify-center">{{ weeks }}</p>
-        </div>
-    </div>
-    <div class="calendar-container"> </div> -->
-    <section class="section w-full py-60 flex justify-center">
         <div class="container flex flex-col justify-center items-center">
             <h2 class="subtitle mb-12">
                 <button class="button is-small is-primary is-outlined mr-5" @click="calendarData(-1)">&lt;</button>
@@ -19,28 +16,57 @@
             <div class="w-full h-100 flex flex-rows px-16 justify-between">
                 <p  v-for="weeks in week" :key="weeks" class="">{{ weeks }}</p>
             </div>
-            <table class="table has-text-centered is-fullwidth table-auto">
+            <table class="table">
                 <thead>
                     <th v-for="day in days" :key="day">{{ day }}</th>
                 </thead>
-                <tbody>
-                    <tr v-for="(date, idx) in dates" :key="idx">
-                        <td v-for="(day, secondIdx) in date" 
-                        :key="secondIdx" 
-                        :class="{ 'text-white': (idx === 0 && day >= lastMonthStart) || (dates.length - 1 === idx && nextMonthStart > day),
-                                'has-text-primary': day === today && month === currentMonth && year === currentYear }"
-                        class="w-48 h-20"
+                <tbody class="w-full">
+                    <tr v-for="(date, rowIndex) in dates" :key="rowIndex" class="w-full flex">
+                        <td v-for="(day, colIndex) in date"  
+                        :key="colIndex" 
+                        :style="{ 'color': isOutsideMonth(rowIndex, colIndex) ? 'text-white' : (isToday(day) ? 'text-black' : ''), 'selected': clickDay == day ? 'blue' : '' }"
+                        class="w-20 h-20 flex justify-center items-center "
+                        :class="{
+                                    'text-white': isOutsideMonth(rowIndex, colIndex),
+                                    'text-black': isToday(day),
+                                    'selected': clickDay == day ? 'blue' : '',
+                                    'text-gray-400': isPastDay(day),
+                                }"
+                        @click="handleDateClick(day)"
                         >
-                            <p class="w-full h-full flex justify-center items-center">{{ day }}</p>
+                            <p class="after:absolute after:w-2 after:h-2 after:bg-black " 
+                            :class="[clickDay == day ? 'after:block' : 'after:hidden']"
+                            >{{ day }}</p>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </section>
+
+    <!-- <tbody class="w-full">
+        <tr v-for="(date, idx) in dates" :key="idx" class="w-full flex">
+            <td v-for="(day, secondIdx) in date" 
+            :key="secondIdx" 
+            :class="{ 'text-white': (idx === 0 && day >= lastMonthStart) || (dates.length - 1 === idx && nextMonthStart > day),
+                    'has-text-primary': day === today && month === currentMonth && year === currentYear }"
+            class="w-20 h-20 flex justify-center items-center "
+            @click="handleDateClick(day)"
+            >
+                <p class="after:absolute after:w-2 after:h-2 after:bg-black " 
+                :class="[clickDay == day ? 'after:block' : 'after:hidden']"
+                >{{ day }}</p>
+            </td>
+        </tr>
+    </tbody> -->
+    <!-- :class="{ 'text-white': isOutsideMonth(rowIndex, colIndex),
+                                    'black-333': isToday(day),
+                                    'selected' : clickDay == day }" -->
+                
 </template>
 
 <script>
+import { ref } from "vue";
 import * as DateData from '../utils/date';
 // import * as Date from './Calendarsc';
 
@@ -49,9 +75,6 @@ export default {
     data() {
         return {
             week: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
-            // calendarInnerHtml : Date.calendarInnerHtml,
-            // currentMonth : Date.currentMonth,
-            // currentYear : Date.currentYear,
             dates: [],
             currentYear: 0,
             year: 0,
@@ -59,6 +82,7 @@ export default {
             lastMonthStart: 0,
             nextMonthStart: 0,
             today: 0,
+            clickDay : null,
         }
     },
     created() {
@@ -93,49 +117,106 @@ export default {
             this.dates = this.getMonthOfDays(monthFirstDay, monthLastDate, lastMonthLastDate)
         },
         getFirstDayLastDate(year, month) {
-            const firstDay = new Date(year, month - 1, 1).getDay() // 이번 달 시작 요일
-            const lastDate = new Date(year, month, 0).getDate() // 이번 달 마지막 날짜
-            let lastYear = year
-            let lastMonth = month - 1
-            if (month === 1) {
-                lastMonth = 12
-                lastYear -= 1
+            const firstDay = new Date(year, month - 1, 1).getDay(); // 이번 달 시작 요일
+            const lastDate = new Date(year, month, 0).getDate(); // 이번 달 마지막 날짜
+            let lastYear = year;
+            let lastMonth = month - 1;
+
+            if (month === 0) {
+                // 1월인 경우에는 작년 12월로 설정
+                lastMonth = 12;
+                lastYear -= 1;
             }
-            const prevLastDate = new Date(lastYear, lastMonth, 0).getDate() // 지난 달 마지막 날짜
-            return [firstDay, lastDate, prevLastDate]
+
+            const prevLastDate = new Date(lastYear, lastMonth, 0).getDate(); // 지난 달 마지막 날짜
+            return [firstDay, lastDate, prevLastDate];
         },
         getMonthOfDays(monthFirstDay, monthLastDate, prevMonthLastDate) {
-            let day = 1
-            let prevDay = prevMonthLastDate - monthFirstDay + 1
-            const dates = []
-            let weekOfDays = []
+            let day = 1;
+            let prevDay = prevMonthLastDate - monthFirstDay + 1;
+            const dates = [];
+            let weekOfDays = [];
+
             while (day <= monthLastDate) {
-                if (day === 1) {
-                    // 1일이 어느 요일인지에 따라 테이블에 그리기 위한 지난 셀의 날짜들을 구할 필요가 있다.
+                if (day === 1 && monthFirstDay !== 0) {
+                    // 이 부분을 수정하여 현재 월이 시작되기 전의 날짜는 추가하지 않도록 함
                     for (let j = 0; j < monthFirstDay; j += 1) {
-                        if (j === 0) this.lastMonthStart = prevDay // 지난 달에서 제일 작은 날짜
-                        weekOfDays.push(prevDay)
-                        prevDay += 1
+                        weekOfDays.push(prevDay);
+                        prevDay += 1;
                     }
                 }
-                weekOfDays.push(day)
+                weekOfDays.push(day);
+
                 if (weekOfDays.length === 7) {
-                    // 일주일 채우면
-                    dates.push(weekOfDays)
-                    weekOfDays = [] // 초기화
+                dates.push(weekOfDays);
+                weekOfDays = [];
                 }
-                day += 1
+
+                day += 1;
             }
-            const len = weekOfDays.length
-            if (len > 0 && len < 7) {
-                for (let k = 1; k <= 7 - len; k += 1) {
-                    weekOfDays.push(k)
-                }
-            }
-            if (weekOfDays.length > 0) dates.push(weekOfDays) // 남은 날짜 추가
-            this.nextMonthStart = weekOfDays[0] // 이번 달 마지막 주에서 제일 작은 날짜
-            return dates
+
+            // 남은 날짜를 마지막 주에 추가하는 부분
+            if (weekOfDays.length > 0) dates.push(weekOfDays);
+
+            return dates;
         },
+        isOutsideMonth(rowIndex, colIndex) {
+            // 함수를 사용하여 현재 월의 시작과 끝을 확인하고 셀이 현재 월에 속하는지 여부를 반환
+            const firstDayOfMonth = new Date(this.currentYear, this.currentMonth, 1).getDay();
+            const lastDayOfMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDay();
+
+            // 현재 월의 시작과 끝 인덱스를 계산
+            const startIndex = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1;
+            const endIndex = lastDayOfMonth === 0 ? 6 : lastDayOfMonth - 1;
+
+            // 현재 셀이 현재 월에 속하는지 여부를 판단
+            return (rowIndex === 0 && colIndex < startIndex) || (rowIndex === 5 && colIndex > endIndex);
+        },
+        isToday(day) {
+            // 함수를 사용하여 현재 날짜인지 여부를 반환
+            const today = new Date();
+            const currentMonthFirstDay = new Date(this.currentYear, this.currentMonth - 1, 1);
+            const lastMonthLastDay = new Date(this.currentYear, this.currentMonth, 0);
+
+            return (
+                today.getDate() === day &&
+                today.getMonth() === this.currentMonth &&
+                today.getFullYear() === this.currentYear &&
+                today >= currentMonthFirstDay &&
+                today <= lastMonthLastDay
+            );
+        },
+        handleDateClick(day, rowIndex, colIndex) {
+            // 클릭한 날짜를 추적하거나 원하는 동작 수행
+            if (!this.isOutsideMonth(rowIndex, colIndex) && !this.isPastDay(day) ) {
+                // 클릭한 날짜가 현재 월에 속하면 저장
+                this.clickDay = day;
+                console.log(`클릭한 날짜: ${day}`);
+            } else {
+                // 클릭한 날짜가 현재 월에 속하지 않으면 아무 동작도 수행하지 않음
+                console.log(`클릭한 날짜 (${day})는 현재 월에 속하지 않습니다.`);
+            }
+            
+        },
+        isPastDay(day) {
+            // 오늘을 기준으로 지난 날짜인지 여부를 반환
+            const today = new Date();
+            const currentMonthFirstDay = new Date(
+                this.currentYear,
+                this.currentMonth - 1,
+                1
+            );
+            let lastMonthLastDay = new Date(this.currentYear, this.currentMonth, 0);
+
+            const currentDate = new Date(
+                this.currentYear,
+                this.currentMonth - 1,
+                day +1
+            );
+
+            return currentDate < today && currentDate >= currentMonthFirstDay;
+        },
+
     },
 }
 </script>
