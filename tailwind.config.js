6f6bb12d9c9fb91526a0326e557b1f84afe8c923
taedonn <taedonn@taedonn.com>
2024-01-18 21:11:47 +0900

    Update timeline

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'gray-f': '#F1F4F9',
         'gray-e': '#E9EAEE',
-        'gray-d': '#D2D4DC',
         'gray-ccc': '#CCCCCC',
         'gray-999': '#999999',
         'gray-666': '#666666',
@@ -15,7 +13,6 @@ export default {
         'blue-2': '#AAD8FB',
         'blue-3': '#7FC4F8',
         'blue-4': '#2A9DF4',
-        'blue-e': '#EDF6FF',
       },
       animation: {
         "fade-in": "fade-in 0.2s 1 both",
