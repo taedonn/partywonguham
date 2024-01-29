d34af698470cdb42bff9afe5efda54d326a6a1ee
taedonn <taedonn@taedonn.com>
2024-01-30 00:09:06 +0900

    버튼 컴포넌트에 MouseEvent 추가

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,8 @@
 <template>
     <button
         @click="handleClick()"
+        @mousedown="handleMousedown"
+        @mouseup="handleMouseup"
         class="px-4 py-2.5 shrink-0 rounded border-2 duration-100 border-blue-4 focus:border-blue-4 text-blue-4 hover:bg-blue-4 hover:text-white"
     > 
         <i
@@ -23,6 +25,15 @@
     });
 
     const handleClick = props.click ? props.click : () => { return }
+    const handleMousedown = (e: MouseEvent) => {
+        const button = e.target as HTMLButtonElement;
+        button.classList.remove("bg-blue-4");
+        button.classList.add("bg-blue-3");
+    }
+    const handleMouseup = (e: MouseEvent) => {
+        const button = e.target as HTMLButtonElement;
+        button.classList.remove("bg-blue-3");
+    }
 </script>
 
 <style></style>
