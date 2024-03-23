76657aa18f742c493afb9a1d16c99df8d93a86b3
taedonn <taedonn@taedonn.com>
2024-03-23 23:04:53 +0900

    Update create page middleware

--- middleware/create.ts
+++ middleware/create.ts
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware(to => {
     if (to.path === "/create") {
-        if (Number(to.query.step) <= 1 || Number(to.query.step) > 3) {
+        if (
+            to.query.step !== "1" &&
+            to.query.step !== "2" &&
+            to.query.step !== "3"
+        ) {
             return navigateTo("/");
         }
     }
