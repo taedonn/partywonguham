38ce26ba6848f03e24f230652f53aba9deff2d0b
taedonn <taedonn@taedonn.com>
2024-03-27 01:34:11 +0900

    Update create page

--- pages/create.vue
+++ pages/create.vue
@@ -89,9 +89,96 @@
             >
                 <div class="w-full text-center">
                     <h2 class="font-bold text-2xl leading-normal">
-                        시간 선택
+                        어느 시간대로 할까요?
                     </h2>
-                    <h3 class="mt-4 mb-10 leading-normal">약속 시간</h3>
+                    <h3 class="mt-4 mb-10 leading-normal">대략적인 시간대를 정해주세요</h3>
+                    <div class="w-full mt-2 flex items-center gap-2">
+                        <input type="radio" name="time" id="time-default" value="default" v-on:change="handleTime" v-bind:checked="states.checkedTime === 'default'" class="peer hidden"/>
+                        <label for="time-default" class="group w-full h-11 px-4 gap-2 flex items-center cursor-pointer rounded-lg border border-gray-6 peer-checked:border-orange-f6 selection:bg-transparent">
+                            <div class="text-lg">
+                                <i class="peer-checked:group-[]:hidden block fa-regular fa-square-check"></i>
+                                <i class="peer-checked:group-[]:block hidden text-orange-f6 fa-solid fa-square-check"></i>
+                            </div>
+                            &#127970;기본 [오전 9시 ~ 오후 6시]
+                        </label>
+                    </div>
+                    <div class="w-full mt-2 flex items-center gap-2">
+                        <input type="radio" name="time" id="time-morning" value="morning" v-on:change="handleTime" v-bind:checked="states.checkedTime === 'morning'" class="peer hidden"/>
+                        <label for="time-morning" class="group w-full h-11 px-4 gap-2 flex items-center cursor-pointer rounded-lg border border-gray-6 peer-checked:border-orange-f6 selection:bg-transparent">
+                            <div class="text-lg">
+                                <i class="peer-checked:group-[]:hidden block fa-regular fa-square-check"></i>
+                                <i class="peer-checked:group-[]:block hidden text-orange-f6 fa-solid fa-square-check"></i>
+                            </div>
+                            &#9925;오전 [오전 8시 ~ 오후 12시]
+                        </label>
+                    </div>
+                    <div class="w-full mt-2 flex items-center gap-2">
+                        <input type="radio" name="time" id="time-afternoon" value="afternoon" v-on:change="handleTime" v-bind:checked="states.checkedTime === 'afternoon'" class="peer hidden"/>
+                        <label for="time-afternoon" class="group w-full h-11 px-4 gap-2 flex items-center cursor-pointer rounded-lg border border-gray-6 peer-checked:border-orange-f6 selection:bg-transparent">
+                            <div class="text-lg">
+                                <i class="peer-checked:group-[]:hidden block fa-regular fa-square-check"></i>
+                                <i class="peer-checked:group-[]:block hidden text-orange-f6 fa-solid fa-square-check"></i>
+                            </div>
+                            &#127774;오후 [오후 12시 ~ 오후 6시]
+                        </label>
+                    </div>
+                    <div class="w-full mt-2 flex items-center gap-2">
+                        <input type="radio" name="time" id="time-night" value="night" v-on:change="handleTime" v-bind:checked="states.checkedTime === 'night'" class="peer hidden"/>
+                        <label for="time-night" class="group w-full h-11 px-4 gap-2 flex items-center cursor-pointer rounded-lg border border-gray-6 peer-checked:border-orange-f6 selection:bg-transparent">
+                            <div class="text-lg">
+                                <i class="peer-checked:group-[]:hidden block fa-regular fa-square-check"></i>
+                                <i class="peer-checked:group-[]:block hidden text-orange-f6 fa-solid fa-square-check"></i>
+                            </div>
+                            &#127769;저녁 [오후 6시 ~ 오전 12시]
+                        </label>
+                    </div>
+                    <div class="w-full mt-2 flex items-center gap-2">
+                        <input type="radio" name="time" id="time-select" value="select" v-on:change="handleTime" v-bind:checked="states.checkedTime === 'select'" class="peer hidden"/>
+                        <label for="time-select" class="group w-full h-11 px-4 gap-2 flex items-center cursor-pointer rounded-lg border border-gray-6 peer-checked:border-orange-f6 selection:bg-transparent">
+                            <div class="text-lg">
+                                <i class="peer-checked:group-[]:hidden block fa-regular fa-square-check"></i>
+                                <i class="peer-checked:group-[]:block hidden text-orange-f6 fa-solid fa-square-check"></i>
+                            </div>
+                            직접 입력
+                        </label>
+                    </div>
+                    <div v-if="states.checkedTime === 'select'" v-bind:class="`${states.checkedTimeState.type === 'error' ? 'animate-shake' : ''} w-full mt-2 relative flex items-center gap-2`">
+                        <div class="w-[5.5rem] shrink-0">
+                            <input v-on:change="handleStartTimeSelect" type="checkbox" id="start-time" class="hidden peer"/>
+                            <label for="start-time" v-on:mousedown="handleStartTimeSelectMouseDown" class="start-time-select group relative w-full h-11 px-4 flex cursor-pointer rounded-lg border border-gray-6 placeholder-gray-9">
+                                <div class="start-time-select relative w-full h-full flex items-center selection:bg-transparent">
+                                    {{ states.startTimeSelect }}
+                                    <i class="start-time-select peer-checked:group-[]:rotate-180 fa-solid fa-angle-down absolute right-0 top-1/2 -translate-y-1/2"></i>
+                                </div>
+                                <ul v-if="states.startTimeSelectShow" class="start-time-select w-[calc(100%+2px)] overflow-y-auto py-2 absolute z-10 -left-px top-12 text-left rounded-lg border border-gray-6 bg-white">
+                                    <li v-on:click="handleStartTimeSelectClick" data-option="오전" class="start-time-select w-full px-4 py-1.5 lg:hover:bg-orange-fe selection:bg-transparent">오전</li>
+                                    <li v-on:click="handleStartTimeSelectClick" data-option="오후" class="start-time-select w-full px-4 py-1.5 lg:hover:bg-orange-fe selection:bg-transparent">오후</li>
+                                </ul>
+                            </label>
+                        </div>
+                        <input type="number" placeholder="ex) 12" v-on:input="handleStartTimeNumChange" id="start-time-num" ref="start-time-num" v-bind:class="`${states.capacityState.type === 'error' ? 'animate-shake' : ''} w-full px-4 py-2.5 border rounded-lg border-gray-6 disabled:border-gray-c placeholder-gray-9`"/>
+                        <div class="shrink-0">부터</div>
+                    </div>
+                    <div v-if="states.checkedTime === 'select'" v-bind:class="`${states.checkedTimeState.type === 'error' ? 'animate-shake' : ''} w-full mt-2 relative flex items-center gap-2`">
+                        <div class="w-[5.5rem] shrink-0">
+                            <input v-on:change="handleEndTimeSelect" type="checkbox" id="end-time" class="hidden peer"/>
+                            <label for="end-time" v-on:mousedown="handleEndTimeSelectMouseDown" class="end-time-select group relative w-full h-11 px-4 flex cursor-pointer rounded-lg border border-gray-6 placeholder-gray-9">
+                                <div class="end-time-select relative w-full h-full flex items-center selection:bg-transparent">
+                                    {{ states.endTimeSelect }}
+                                    <i class="end-time-select peer-checked:group-[]:rotate-180 fa-solid fa-angle-down absolute right-0 top-1/2 -translate-y-1/2"></i>
+                                </div>
+                                <ul v-if="states.endTimeSelectShow" class="end-time-select w-[calc(100%+2px)] overflow-y-auto py-2 absolute z-10 -left-px top-12 text-left rounded-lg border border-gray-6 bg-white">
+                                    <li v-on:click="handleEndTimeSelectClick" data-option="오전" class="end-time-select w-full px-4 py-1.5 lg:hover:bg-orange-fe selection:bg-transparent">오전</li>
+                                    <li v-on:click="handleEndTimeSelectClick" data-option="오후" class="end-time-select w-full px-4 py-1.5 lg:hover:bg-orange-fe selection:bg-transparent">오후</li>
+                                </ul>
+                            </label>
+                        </div>
+                        <input type="number" placeholder="ex) 6" v-on:input="handleEndTimeNumChange" id="end-time-num" ref="end-time-num" v-bind:class="`${states.capacityState.type === 'error' ? 'animate-shake' : ''} w-full px-4 py-2.5 border rounded-lg border-gray-6 disabled:border-gray-c placeholder-gray-9`"/>
+                        <div class="shrink-0">까지</div>
+                    </div>
+                    <div v-if="states.checkedTimeState.type === 'error'" class="mt-2 text-xs text-left text-red-e">
+                        {{ states.checkedTimeState.msg }}
+                    </div>
                     <div class="w-full h-12 mt-4">
                         <Button :click="handleStepForward" :isLoading="states.isLoading">{{ states.allowCapacity ? '다음으로' : '약속 생성' }}</Button>
                     </div>
@@ -168,6 +255,10 @@
     // Types
     import type { Time, State } from '~/utils/global.d';
 
+    // Route
+    const route = useRoute();
+    const router = useRouter();
+
     interface Table {
         date: string | Date,
         times: Time[]
@@ -181,17 +272,19 @@
         capacityState: State,
         allowCapacity: boolean,
         dates: string[],
-        start_time: number,
-        end_time: number,
+        startTime: number,
+        endTime: number,
+        checkedTime: string,
+        checkedTimeState: State,
+        startTimeSelect: string,
+        startTimeSelectShow: boolean,
+        endTimeSelect: string,
+        endTimeSelectShow: boolean,
         allowEmail: boolean,
         email: string,
         emailState: State,
         isLoading: boolean,
     }
-
-    // Route
-    const route = useRoute();
-    const router = useRouter();
 
     // States
     const states: States = reactive({
@@ -206,8 +299,14 @@
             'Wed Dec 27 2023',
             'Thu Dec 28 2023',
         ],
-        start_time: 10,
-        end_time: 15,
+        startTime: 9,
+        endTime: 18,
+        checkedTime: "default",
+        checkedTimeState: { type: "", msg: "" },
+        startTimeSelect: "오전",
+        startTimeSelectShow: false,
+        endTimeSelect: "오후",
+        endTimeSelectShow: false,
         allowEmail: false,
         email: "",
         emailState: { type: "", msg: "" },
@@ -268,6 +367,16 @@
                     } else {
                         capacity.value.focus();
                     }
+                } else if (Number(states.capacity) <= 0) {
+                    states.capacityState = { type: "error", msg: "인원수는 0명 이하로 설정할 수 없어요." }
+
+                    // Trigger scroll event
+                    if (width >= 1024) {
+                        capacity.value.focus({ preventScroll: true });
+                        window.scrollTo({ top: 0, behavior: "smooth" });
+                    } else {
+                        capacity.value.focus();
+                    }
                 } else {
                     router.push("/create?step=3");
                 }
@@ -277,11 +386,29 @@
         } else if (states.query === "3") {
             router.push("/create?step=4");
         } else if (states.query === "4") {
-            if (states.allowCapacity) {
-                router.push("/create?step=5");
+            const startTimeNum = document.getElementById("start-time-num") as HTMLInputElement;
+            const endTimeNum = document.getElementById("end-time-num") as HTMLInputElement;
+
+            if (states.checkedTime === "select") {
+                if (startTimeNum.value === "" || endTimeNum.value === "") {
+                    states.checkedTimeState = { type: "error", msg: "시간대를 입력해 주세요." }
+                } else if (states.startTime > 24 || states.startTime < 0 || states.endTime > 24 || states.endTime < 0 || states.endTime - states.startTime <= 0) {
+                    states.checkedTimeState = { type: "error", msg: "시간대를 올바르게 입력해 주세요." }
+                } else {
+                    if (states.allowCapacity) {
+                        router.push("/create?step=5");
+                    } else {
+                        // Init creation
+                        await handleCreate();
+                    }
+                }
             } else {
-                // Init creation
-                await handleCreate();
+                if (states.allowCapacity) {
+                    router.push("/create?step=5");
+                } else {
+                    // Init creation
+                    await handleCreate();
+                }
             }
         } else if (states.query === "5") {
             if (states.allowEmail) {
@@ -327,6 +454,87 @@
         states.capacityState.type = "";
     }
 
+    /** Trigger time check event */
+    const handleTime = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.checkedTime = el.value;
+        states.checkedTimeState = { type: "", msg: "" };
+
+        if (states.checkedTime === "morning") {
+            states.startTime = 8;
+            states.endTime = 12;
+        } else if (states.checkedTime === "afternoon") {
+            states.startTime = 12;
+            states.endTime = 18;
+        } else if (states.checkedTime === "night") {
+            states.startTime = 18;
+            states.endTime = 24;
+        }
+    }
+
+    const handleStartTimeSelect = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.startTimeSelectShow = el.checked;
+    }
+
+    const handleStartTimeSelectClick = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.startTimeSelect = el.getAttribute("data-option") + "";
+    }
+
+    const handleStartTimeSelectMouseDown = (e: MouseEvent) => {
+        const el = e.target as HTMLElement;
+        const select = document.getElementById("start-time") as HTMLInputElement;
+        if (!el.classList.contains("start-time-select")) {
+            if (select) {
+                select.checked = false;
+                states.startTimeSelectShow = false;
+            }
+        }
+    }
+
+    const handleStartTimeNumChange = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.checkedTimeState = { type: "", msg: "" };
+        if (states.startTimeSelect === "오전") {
+            states.startTime = Number(el.value);
+        } else {
+            if (Number(el.value) === 12) states.startTime = Number(el.value);
+            else if (Number(el.value)) states.startTime = Number(el.value) + 12;
+        }
+    }
+
+    const handleEndTimeSelect = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.endTimeSelectShow = el.checked;
+    }
+
+    const handleEndTimeSelectClick = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.endTimeSelect = el.getAttribute("data-option") + "";
+    }
+
+    const handleEndTimeSelectMouseDown = (e: MouseEvent) => {
+        const el = e.target as HTMLElement;
+        const select = document.getElementById("end-time") as HTMLInputElement;
+        if (!el.classList.contains("end-time-select")) {
+            if (select) {
+                select.checked = false;
+                states.endTimeSelectShow = false;
+            }
+        }
+    }
+
+    const handleEndTimeNumChange = (e: Event) => {
+        const el = e.target as HTMLInputElement;
+        states.checkedTimeState = { type: "", msg: "" };
+        if (states.endTimeSelect === "오전") {
+            states.endTime = Number(el.value);
+        } else {
+            states.endTime = Number(el.value) + 12;
+        }
+    }
+
     /** Trigger email check event */
     const handleEmail = (e: Event) => {
         const el = e.target as HTMLInputElement;
@@ -358,7 +566,7 @@
         
         // 테이블 생성
         const thisTable = [];
-        const thisTime = handleTimeBlockArr(states.start_time, states.end_time);
+        const thisTime = handleTimeBlockArr(states.startTime, states.endTime);
         for (let i = 0; i < states.dates.length; i++) {
             let obj: Table = { date: states.dates[i], times: [] };
             for (let j = 0; j < thisTime.length; j++) {
@@ -374,8 +582,8 @@
         const data = {
             title: states.title === "" ? "제목없음" : states.title,
             dates: states.dates,
-            start_time: states.start_time,
-            end_time: states.end_time,
+            start_time: states.startTime,
+            end_time: states.endTime,
             email: states.email,
             allow_email: states.email === "" ? false : states.allowEmail,
             partywons: [],
@@ -395,6 +603,8 @@
 
     onMounted(() => {
         window.addEventListener("keydown", onGlobalStepForward);
+        window.addEventListener("mousedown", handleStartTimeSelectMouseDown);
+        window.addEventListener("mousedown", handleEndTimeSelectMouseDown);
 
         // Focus input on component mount
         if (!states.query) {
@@ -410,5 +620,7 @@
 
     onBeforeUnmount(() => {
         window.removeEventListener("keydown", onGlobalStepForward);
+        window.removeEventListener("mousedown", handleStartTimeSelectMouseDown);
+        window.removeEventListener("mousedown", handleEndTimeSelectMouseDown);
     });
 </script>
